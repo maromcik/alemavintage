d39a81af00e813977d64f554346181edbea648ee
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-12-20 00:22:21 +0100

    deployment

--- podman.sh
+++ podman.sh
@@ -1,3 +1,5 @@
 podman login cerit.io
 podman build -t cerit.io/roman_alexander_mariancik/alemavintage-image .
+podman tag cerit.io/roman_alexander_mariancik/alemavintage-image 192.168.1.33:5000/alemavintage-image
 podman push cerit.io/roman_alexander_mariancik/alemavintage-image:latest
+podman push 192.168.1.33:5000/alemavintage-image
