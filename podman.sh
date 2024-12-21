a449e48bbaf225edc81c339432df1f85486d4058
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-12-21 11:36:05 +0100

    changed deployment script

--- podman.sh
+++ podman.sh
@@ -3,3 +3,5 @@ podman build -t cerit.io/roman_alexander_mariancik/alemavintage-image .
 podman tag cerit.io/roman_alexander_mariancik/alemavintage-image 192.168.1.33:5000/alemavintage-image
 podman push cerit.io/roman_alexander_mariancik/alemavintage-image:latest
 podman push 192.168.1.33:5000/alemavintage-image
+ssh roman@server systemctl --user restart alemavintage.service
+ssh roman@hp systemctl --user restart alemavintage.service
