f1618b52e96ec05dec739a3ac48771c5579cf18c
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-12-23 11:13:28 +0100

    feat(login):

--- podman.sh
+++ podman.sh
@@ -5,3 +5,4 @@ podman push cerit.io/roman_alexander_mariancik/alemavintage-image:latest
 podman push 192.168.1.33:5000/alemavintage-image
 ssh roman@server systemctl --user restart alemavintage.service
 ssh roman@hp systemctl --user restart alemavintage.service
+kubectl apply -f kubernetes/alemavintage -n mariancik-ns
