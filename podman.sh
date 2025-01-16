b2e34b4b2c6dcc9623000223fbcf69fbb2c4133d
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2025-01-16 22:05:56 +0100

    feat(podman): changed ip

--- podman.sh
+++ podman.sh
@@ -1,8 +1,8 @@
 #podman login cerit.io
-podman build -t 192.168.1.10:5000/alemavintage-image .
+podman build -t 192.168.0.10:5000/alemavintage-image .
 #podman tag 192.168.1.10:5000/alemavintage-image cerit.io/roman_alexander_mariancik/alemavintage-image
 #podman push cerit.io/roman_alexander_mariancik/alemavintage-image:latest
-podman push 192.168.1.10:5000/alemavintage-image
+podman push 192.168.0.10:5000/alemavintage-image
 ssh roman@server systemctl --user restart alemavintage.service
 ssh roman@hp systemctl --user restart alemavintage.service
 #kubectl apply -f kubernetes/alemavintage -n mariancik-ns
