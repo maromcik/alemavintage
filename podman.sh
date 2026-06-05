636ab73a7f0788f991a36f9987798ecce67623e9
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2026-06-05 17:19:58 +0200

    feat: darkmode only

--- podman.sh
+++ podman.sh
@@ -4,5 +4,5 @@ podman build -t 192.168.0.10:5000/alemavintage-image .
 #podman push cerit.io/roman_alexander_mariancik/alemavintage-image:latest
 podman push 192.168.0.10:5000/alemavintage-image
 ssh roman@server systemctl --user restart alemavintage.service
-ssh roman@hp systemctl --user restart alemavintage.service
+# ssh roman@hp systemctl --user restart alemavintage.service
 #kubectl apply -f kubernetes/alemavintage -n mariancik-ns
